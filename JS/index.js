2eb6be8e8ab057f4c6bbbf803329fc1c994e2d55
Johandro Gonzalez <88999789+JohandroG@users.noreply.github.com>
2021-09-15 09:21:11 -0600

    New changes

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+let listElements= document.querySelectorAll("li");
+console.log(listElements);
+
+let formElements = document.querySelectorAll(".formElement");
+console.log(formElements);
+
+let country = document.querySelector("#userCountry");
+console.log(country);
+
+country.value
